0fa3d77fe8acf86160c1f6abc74da80bff6707f1
Wer Pietrzak <95078050+werpietrzak@users.noreply.github.com>
2022-11-21 16:30:36 +0100

    feat: add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmployeeListComponent} from "./ui/employee-list/employee-list.component";
+import {EmployeeServiceModule} from "./services/employee/employee.service-module";
+import {EmployeeListComponentModule} from "./ui/employee-list/employee-list.component-module";
+import {EmployeeFormComponent} from "./ui/employee-form/employee-form.component";
+import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
+import {HomeComponent} from "./ui/home/home.component";
+import {HomeComponentModule} from "./ui/home/home.component-module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'employee-list',
+    component: EmployeeListComponent,
+  },
+  {
+    path: 'create-employee',
+    component: EmployeeFormComponent,
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeComponentModule,
+    EmployeeListComponentModule,
+    EmployeeFormComponentModule,
+    EmployeeServiceModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
